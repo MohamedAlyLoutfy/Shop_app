4f1c9d34d7c1ebd85e8a86980d7688aea10d4720
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-28 14:36:03 +0200

    connecting the cart provider

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -14,10 +14,14 @@ CartItem({
 });
 }
 class Cart with ChangeNotifier{
-  Map<String ,CartItem> _items;
+  Map<String ,CartItem> _items={};
   Map<String ,CartItem> get items{
     return{..._items};
   }
+  int get  itemCount{
+    return  _items.length;
+  }
+
   
   void addItem(String productId,double price,String title,){
     if (_items.containsKey(productId)){
@@ -44,7 +48,9 @@ class Cart with ChangeNotifier{
           ),
           );
     }
+    notifyListeners();
   }
+ 
 
 
 
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,7 +3,8 @@ import 'package:provider/provider.dart';
 import '../providers/product.dart';
 import '../widgets/productsgrid.dart';
 import '../providers/products.dart';
-
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions{
   Favorties,
@@ -52,6 +53,18 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             ],
 
           ),
+          Consumer<Cart>(builder:(_,cart,ch)=>Badge(
+            child: ch,
+             value: cart.itemCount.toString(),
+             ),
+             child:IconButton(
+              icon:Icon(
+                Icons.shopping_cart,
+              ) ,
+              onPressed: (){},
+              ) ,
+             ) ,
+
         ],
         ),
         body: ProductsGrid(_showOnlyFavorites),
