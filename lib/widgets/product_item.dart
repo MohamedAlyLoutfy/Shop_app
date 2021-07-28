79b377f918246d8d5fdd502ebbdea2a14e01da5a
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-28 11:54:11 +0200

    using consumer instead of provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -11,48 +11,51 @@ class ProductItem  extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product =Provider.of<Product>(context);
+    final product =Provider.of<Product>(context,listen:false);
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: (){
-            Navigator.of(context)
-            .pushNamed(
-              ProductDetailScreen.routeName,
-              arguments:product.id, );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-            
-            ),
-        ),
-    
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite? Icons.favorite
-              :Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteState();
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: (){
+              Navigator.of(context)
+              .pushNamed(
+                ProductDetailScreen.routeName,
+                arguments:product.id, );
             },
-            
-            ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-              icon:Icon(Icons.shopping_cart,),
-               onPressed: () {},
-               color: Theme.of(context).accentColor,
-    
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+              
               ),
-            ),
-      ),
+          ),
+      
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading:Consumer<Product>(
+      builder: (ctx,product,_)=> IconButton(
+              icon: Icon(
+                product.isFavorite? Icons.favorite
+                :Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteState();
+              },
+              
+              ),),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+              ),
+              trailing: IconButton(
+                icon:Icon(Icons.shopping_cart,),
+                 onPressed: () {},
+                 color: Theme.of(context).accentColor,
+      
+                ),
+              ),
+        ),
+      
+      
     );
   }
 }
