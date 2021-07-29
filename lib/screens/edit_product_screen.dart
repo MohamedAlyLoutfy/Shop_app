5ee1f253919c938808eb59d81450c2d56842ee65
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-07-29 14:04:10 +0200

    Using Forms and working with forms input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
  class EditProductScreen  extends StatefulWidget {
+   static const routeName='/edit-product';
  
 
   @override
@@ -12,6 +13,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title:Text('Edit Product') ,
+        ),
+        body: Padding(
+          padding:const EdgeInsets.all(16.0) ,
+          child: Form(child: ListView(children:<Widget> [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Title'),
+              textInputAction: TextInputAction.next,
+        
+        
+            ),
+        
+        
+        
+          ],),),
         ),
       
     );
