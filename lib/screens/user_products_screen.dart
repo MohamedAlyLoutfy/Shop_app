e8f15a623df5d95a1749b3f39759c030a40c1a8f
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-01 13:20:43 +0200

    pull to refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/product.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
@@ -6,6 +7,9 @@ import '../widgets/app_drawer.dart';
 import './edit_product_screen.dart';
 class UserProductsScreen  extends StatelessWidget {
   static const routeName='/user-products';
+  Future<void> _refreshProducts(BuildContext context)async{
+    await Provider.of<Products>(context,listen: false).fetchAndSetProducts();
+  }
 
  
 
@@ -27,23 +31,26 @@ class UserProductsScreen  extends StatelessWidget {
         
         ),
         drawer: AppDrawer(),
-        body: Padding(
-          padding:EdgeInsets.all(8) ,
-          child:ListView.builder(
-            itemCount:productsData.items.length,
-            itemBuilder: (_,i)=> Column(
-              children:[ UserProductItem(
-                productsData.items[i].id,
-                productsData.items[i].title,
-                productsData.items[i].imageUrl,
-            
-            
+        body:  RefreshIndicator(
+          onRefresh:()=>_refreshProducts(context) ,
+          child: Padding(
+            padding:EdgeInsets.all(8) ,
+            child:ListView.builder(
+              itemCount:productsData.items.length,
+              itemBuilder: (_,i)=> Column(
+                children:[ UserProductItem(
+                  productsData.items[i].id,
+                  productsData.items[i].title,
+                  productsData.items[i].imageUrl,
+              
+              
+                ),
+                Divider(),
+                ],
               ),
-              Divider(),
-              ],
+              ),
             ),
-            ),
-          ),
+        ),
       
     );
   }
