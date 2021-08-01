d85148a3f365258fa3364b9af5f36dab18c1f68f
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-01 15:57:18 +0200

    update favorite statues optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
-
+import 'package:http/http.dart' as http;
 class Product with ChangeNotifier{
   final String id;
   final String title;
@@ -16,9 +17,28 @@ class Product with ChangeNotifier{
      @required this.imageUrl,
      this.isFavorite=false,
   });
-  void toggleFavoriteState(){
-    isFavorite=!isFavorite;
+  void _setFavValue(bool newValue){
+    isFavorite=newValue;
     notifyListeners();
   }
+  Future <void> toggleFavoriteState ()async{
+    final oldStatus=isFavorite;
+    isFavorite=!isFavorite;
+    notifyListeners();
+    final url='https://flutter-update-43761-default-rtdb.firebaseio.com/products/$id.json';
+    try{
+   final response= await http.patch(Uri.parse(url),
+    body: json.encode({
+      'isFavorite':isFavorite,
+    }),
+    );
+    if(response.statusCode>=400){
+   _setFavValue(oldStatus);
 
+    }
+  }catch (error){
+   _setFavValue(oldStatus);
+
+  }
+  }
 }
