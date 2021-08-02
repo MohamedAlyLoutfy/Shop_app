b4731367d53a4f5fced5ea5244e7a9ba09728598
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-02 12:17:52 +0200

    allowing users to login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,8 +5,10 @@ class Auth with ChangeNotifier{
   String _token;
   DateTime _expiryDate;
   String _userId;
-  Future <void> signup(String email,String password) async{
-    const url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA53-LYq9cVFcuKUmcYDQCwQt89LAguKqg';
+
+
+  Future <void> _authenticate(String email,String password,String  urlSegment)async{
+    final url='https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA53-LYq9cVFcuKUmcYDQCwQt89LAguKqg';
     final response=await http.post(Uri.parse(url),
     body: json.encode(
       {
@@ -19,4 +21,19 @@ class Auth with ChangeNotifier{
     print(json.decode(response.body));
 
   }
+
+
+  Future <void> signup(String email,String password) async{
+   return _authenticate(email, password, 'signUp');
+
+
+  }
+
+  Future <void> signin(String email,String password) async{
+    return _authenticate(email, password, 'signInWithPassword');
+
+
+  }
+
+
 }
