029988251590fe60db517481f8361af9ab9f0c13
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-03 11:43:37 +0200

    using the animatedBuilder Widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -117,11 +117,11 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
     (CurvedAnimation(
       parent: _controller,curve:Curves.fastOutSlowIn ),
     );
-    _heightAnimation.addListener(() { 
-      setState(() {
+    // _heightAnimation.addListener(() { 
+    //   setState(() {
         
-      });
-    });
+    //   });
+    // });
   }
   @override
   void dispose() {
@@ -222,14 +222,15 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child:
+      AnimatedBuilder(animation:_heightAnimation,builder: (ctx,ch)=> Container(
         //height: _authMode == AuthMode.Signup ? 320 : 260,
         height: _heightAnimation.value.height,
         constraints:
             BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: Form(
+        child:ch),child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
@@ -303,8 +304,9 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
               ],
             ),
           ),
-        ),
-      ),
+        ), ),
+      
+      
     );
   }
 }
